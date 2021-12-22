c50254a75f62e548bbd4fa15e4fe115eed9d91e0
kodoku169 <79566948+kodoku169@users.noreply.github.com>
2021-12-22 12:59:41 +0700

    form-auth

---
+++ main.js
@@ -0,0 +1,8 @@
+const formMain = document.querySelector('#form-signup'),
+    input = formMain.querySelectorAll('.signup-form-group__input'),
+    label = formMain.querySelectorAll('.signup-form-group__label')
+
+
+input.forEach((element, index) => {
+    
+});
